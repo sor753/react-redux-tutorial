2680bc0a3aac669140b13a87c14b3b20781c425e
show kawazoe <show753@icloud.com>
2022-07-02 15:55:09 +0900

    learn prop types

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,12 @@
+import PropTypes from 'prop-types'
+
 function App() {
   const dom = <h1 className="foo">Hi!</h1>
   return (
     <div>
       <User name={"Taro"} age={10} />
       <User name={"Ken"} age={12} />
-      <User name={"Kenta"} />
+      <User name={"Kenta"} age={20} />
     </div>
   )
 }
@@ -12,8 +14,9 @@ function App() {
 const User = (props) => {
   return <div>Hi! {props.name}, {props.age}</div>
 }
-User.defaultProps = {
-  age: 20
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
