99e3b9347dcdaaedf050254984c6a49c3917842b
show kawazoe <show753@icloud.com>
2022-07-02 15:39:49 +0900

    create components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,6 +1,16 @@
 function App() {
   const dom = <h1 className="foo">Hi!</h1>
-  return dom;
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
